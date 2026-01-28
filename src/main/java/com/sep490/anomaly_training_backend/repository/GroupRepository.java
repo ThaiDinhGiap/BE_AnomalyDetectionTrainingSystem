361d3315823e5854e7ff589362c8f8e14821931f
Tung Pham <tungpvhe186583@fpt.edu.vn>
2026-01-28 22:15:07 +0700

    tungpv: List Group By Team Lead or Supervisor

--- src/main/java/com/sep490/anomaly_training_backend/repository/GroupRepository.java
+++ src/main/java/com/sep490/anomaly_training_backend/repository/GroupRepository.java
@@ -4,6 +4,9 @@ import com.sep490.anomaly_training_backend.model.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,7 +14,16 @@ import java.util.Optional;
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     List<Group> findBySectionId(Long sectionId);
+
     boolean existsByName(String name);
+
     Optional<Group> findByIdAndDeleteFlagFalse(Long id);
+
+    List<Group> findBySupervisorId(Long supervisorId);
+
+    @Query("SELECT DISTINCT g from Team t join t.group g where t.teamLeader.id = :teamLeadId AND g.deleteFlag = false")
+    List<Group> findByTeamLeadId(@Param("teamLeadId") Long teamLeadId);
+
+
 }
 
