d55d234ae135fd323b573986c44f9d5e0f17ce8f
Tung Pham <tungpvhe186583@fpt.edu.vn>
2026-03-09 15:31:27 +0700

    change logic Defect Proposal

--- src/main/java/com/sep490/anomaly_training_backend/dto/request/DefectProposalDetailRequest.java
+++ src/main/java/com/sep490/anomaly_training_backend/dto/request/DefectProposalDetailRequest.java
@@ -1,14 +1,14 @@
 package com.sep490.anomaly_training_backend.dto.request;
 
 import com.sep490.anomaly_training_backend.enums.ProposalType;
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 @Builder
-public class CreateDefectProposalDetailRequest {
+public class DefectProposalDetailRequest {
+    Long id;
     Long defectId;
     ProposalType proposalType;
     String defectDescription;
