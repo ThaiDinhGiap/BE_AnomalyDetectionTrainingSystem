34fd3e260c48388293fbf73a8760831fa889592e
giap.thaidinh <giaptdhe186094@fpt.edu.vn>
2026-03-29 23:32:07 +0700

    giaptd/fix_org_api

--- src/main/java/com/sep490/anomaly_training_backend/configuration/SecurityConfig.java
+++ src/main/java/com/sep490/anomaly_training_backend/configuration/SecurityConfig.java
@@ -14,20 +14,20 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config. annotation.web.builders.HttpSecurity;
-import org.springframework. security.config.annotation.web. configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org. springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto. bcrypt.BCryptPasswordEncoder;
-import org.springframework.security. crypto.password.PasswordEncoder;
-import org.springframework.security. web.SecurityFilterChain;
-import org.springframework.security.web. authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework. web.cors.CorsConfiguration;
-import org.springframework.web. cors.CorsConfigurationSource;
-import org.springframework.web. cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java. util.Arrays;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -67,7 +67,8 @@ public class SecurityConfig {
             "/login/oauth2/**",
             "/error",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+            "/ws/**"
     };
 
     @Bean
@@ -107,8 +108,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedMethods(Arrays.asList(allowedMethods.split(",")));
         configuration.setAllowedHeaders(List.of(allowedHeaders));
-        configuration.setExposedHeaders(Arrays.asList(exposedHeaders. split(",")));
-        configuration. setAllowCredentials(allowCredentials);
+        configuration.setExposedHeaders(Arrays.asList(exposedHeaders.split(",")));
+        configuration.setAllowCredentials(allowCredentials);
         configuration.setMaxAge(maxAge);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
